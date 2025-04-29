21c936ee310d57f960d4fb3b4ed15d3b29a2311c
Thurzix <arthurioxgamer@gmail.com>
2025-04-29 14:31:01 -0300

    save 3

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,17 +1,20 @@
+import { BrowserRouter } from "react-router-dom";
+
 export default function AppRoutes() {
     return(
         <>
-        <browserRouter>
+        <BrowserRouter>
             <Link to="/">Home</Link>
             <Link to="/">Detalhes</Link>
             <Link to="/">Favoritos</Link>
-        </browserRouter>
+
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/Detalhes" element={<Detalhes/>} />
+                <Route path="/Favoritos" element={<Favoritos/>} />
+            </Routes>
+        </BrowserRouter>
         </>
     )
-
-
-
-
-
 
 }
