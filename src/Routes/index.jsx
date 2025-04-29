b86764c8ec2b01d11b1815656c6b94f664415df7
Thurzix <135227021+Thurzix@users.noreply.github.com>
2025-04-29 14:16:06 -0300

    save 2

---
+++ src/Routes/index.jsx
@@ -0,0 +1,17 @@
+export default function AppRoutes() {
+    return(
+        <>
+        <browserRouter>
+            <Link to="/">Home</Link>
+            <Link to="/">Detalhes</Link>
+            <Link to="/">Favoritos</Link>
+        </browserRouter>
+        </>
+    )
+
+
+
+
+
+
+}
