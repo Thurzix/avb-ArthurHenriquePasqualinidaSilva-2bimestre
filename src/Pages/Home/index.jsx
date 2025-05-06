681c5a557b2481b6939bb858a25a256d4f9e8559
Thurzix <arthurioxgamer@gmail.com>
2025-05-06 14:08:30 -0300

    adicionado axios no main

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,17 @@
-export default function() {
-    return(
-        <>
-        <h1 className="text 2x1 font-bold">Home</h1>
-        </>
-    )
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10")
+    .then(res => (setPosts(res.data), console.log(res.data)));
+  }, []);
+
+  return (
+    <>
+
+    </>
+  )
 }
